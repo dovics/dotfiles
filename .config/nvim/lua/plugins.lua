3247b30839a0403a622b50f7de9a9ff4c0b91030
dovics <wrs369@88.com>
2024-11-23 12:32:17 +0000

    update nvim config
    Signed-off-by: dovics <wrs369@88.com>

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,206 @@
+-- Install Lazy.nvim automatically if it's not installed(Bootstraping)
+-- Hint: string concatenation is done by `..`
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- After installation, run `checkhealth lazy` to see if everything goes right
+-- Hints:
+--     build: It will be executed when a plugin is installed or updated
+--     config: It will be executed when the plugin loads
+--     event: Lazy-load on event
+--     dependencies: table
+--                   A list of plugin names or plugin specs that should be loaded when the plugin loads.
+--                   Dependencies are always lazy-loaded unless specified otherwise.
+--     ft: Lazy-load on filetype
+--     cmd: Lazy-load on command
+--     init: Functions are always executed during startup
+--     branch: string?
+--             Branch of the repository
+--     main: string?
+--           Specify the main module to use for config() or opts()
+--           , in case it can not be determined automatically.
+--     keys: string? | string[] | LazyKeysSpec table
+--           Lazy-load on key mapping
+--     opts: The table will be passed to the require(...).setup(opts)
+require("lazy").setup({
+	-- LSP manager
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	-- Add hooks to LSP to support Linter && Formatter
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			-- Note:
+			--     the default search path for `require` is ~/.config/nvim/lua
+			--     use a `.` as a path separator
+			--     the suffix `.lua` is not needed
+			require("config.mason-null-ls")
+		end,
+	},
+	-- Vscode-like pictograms
+	{
+		"onsails/lspkind.nvim",
+		event = { "VimEnter" },
+	},
+	-- Auto-completion engine
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"lspkind.nvim",
+			"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+			"hrsh7th/cmp-buffer", -- buffer auto-completion
+			"hrsh7th/cmp-path", -- path auto-completion
+			"hrsh7th/cmp-cmdline", -- cmdline auto-completion
+		},
+		config = function()
+			require("config.nvim-cmp")
+		end,
+	},
+	-- Colorscheme
+    {
+    	"folke/tokyonight.nvim",
+        lazy = true,
+        opts = {
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            }
+        }
+    },
+	-- Better UI
+	-- Run `:checkhealth noice` to check for common issues
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- Add any options here
+		},
+		dependencies = {
+			-- If you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
+	-- Git integration
+	"tpope/vim-fugitive",
+	-- Git decorations
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("config.gitsigns")
+		end,
+	},
+	-- Autopairs: [], (), "", '', etc
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("config.nvim-autopairs")
+		end,
+	},
+	-- Treesitter-integration
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("config.nvim-treesitter")
+		end,
+	},
+	-- Nvim-treesitter text objects
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("config.nvim-treesitter-textobjects")
+		end,
+	},
+	-- Show indentation and blankline
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			require("config.indent-blankline")
+		end,
+	},
+	-- Status line
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("config.lualine")
+		end,
+	},
+	-- Markdown support
+	{ "preservim/vim-markdown", ft = { "markdown" } },
+	-- Markdown previewer
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	-- File explorer
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons", -- optional, for file icons
+		},
+		config = function()
+			require("config.nvim-tree")
+		end,
+	},
+	-- Smart motion
+	-- Usage: Enter a 2-character search pattern then press a label character to
+	--        pick your target.
+	--        Initiate the sesarch with `s`(forward) or `S`(backward)
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			-- See `:h leap-custom-mappings` for more details
+			require("leap").create_default_mappings()
+		end,
+	},
+    -- Better terminal integration
+    {
+        "ainsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("config.toggleterm")
+		end,
+	},
+	-- Fuzzy finder
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("config.telescope")
+		end,
+	},
+	-- Improve the performance of big file
+	{
+		"pteroctopus/faster.nvim",
+	},
+})
