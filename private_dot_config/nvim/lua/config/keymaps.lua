61a1e4423945235bec4840494190af74dec045ff
wangrushen <wrs369@88.com>
2025-01-01 14:20:54 +0800

    fix: nvim config

---
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+local opts = { silent = true, noremap = true }
+
+map("n", "<C-q>", ":q<CR>", opts)
---
+++ private_dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  { "MagicDuck/grug-far.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  --  { "folke/trouble.nvim", enabled = false },
+}
