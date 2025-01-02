af931d33f4d8c88363079db174827d72c8e9779e
wangrushen <wrs369@88.com>
2025-01-02 20:56:54 +0800

    nvim: support golang lsp

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -7,7 +7,7 @@ set -x EDITOR nvim
 # Golang
 set -x GOPROXY https://proxy.golang.com.cn,direct
 set -x PATH $PATH $HOME/go/bin
-
+set -x PATH $PATH /usr/local/go/bin/
 set -g fish_greeting
 
 if status is-interactive
