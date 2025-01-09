4156f2c9e2639155b74e0017e721c1185f1bdad9
wangrushen <wrs369@88.com>
2025-01-09 14:21:05 +0800

    enable golang and python

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -17,13 +17,4 @@ return {
       },
     },
   },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        html = { "prettier" },
-        markdown = { "prettier" },
-      },
-    },
-  },
 }
--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,7 +1,7 @@
 set -x PATH $PATH /opt/homebrew/bin/
 set -x PATH $PATH /opt/homebrew/opt/llvm@12/bin
 set -x PATH $PATH $HOME/bin
-
+set -x PATH $PATH $HOME/miniconda3/bin
 set -x EDITOR nvim
 
 # Golang
@@ -13,3 +13,16 @@ set -g fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/dovics/miniconda3/bin/conda
+    eval /home/dovics/miniconda3/bin/conda "shell.fish" hook $argv | source
+else
+    if test -f "/home/dovics/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/dovics/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH /home/dovics/miniconda3/bin $PATH
+    end
+end
+# <<< conda initialize <<
